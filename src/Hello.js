e8c090ebde2fc544af380bc20b654c880196b5a3
sergiofs-tech <sergiofs.tech@gmail.com>
2021-01-02 00:56:50 +0100

    Example Hello component
    Just a small example to understand the basics

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!!</h1>
+        <p>{this.props.greeting}</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
