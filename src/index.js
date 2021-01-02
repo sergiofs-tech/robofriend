9ac9135ee35500f6b9dac7428c4dc8dd5928f42d
sergiofs-tech <sergiofs.tech@gmail.com>
2021-01-02 01:43:10 +0100

    Basic cards
    Just first step, creation of cards and some basic style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Card from './Card.js';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
