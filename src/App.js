89be141819f5f9e0fc6c69206bde4c194e19d235
sergiofs-tech <sergiofs.tech@gmail.com>
2021-01-03 19:12:16 +0100

    Some style addons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots.js';
+import './App.css'
 
 class App extends Component {
   constructor() {
