94ef4250c4aecc5bcb86bc134fb0d4873e8e604c
Unknown <sergiofs.tech@gmail.com>
2021-07-21 00:39:28 +0200

    Final Robofriends Section 6

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll';
+import SearchBox from '../components/SearchBox';
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        return this.setState({robots: users});
+      });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value});
+  }
+
+  render() {
+    const {robots, searchfield} = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
+    if (robots.length === 0) {
+      return <h1> Please wait... </h1>
+    }
+
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredRobots}/>
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
